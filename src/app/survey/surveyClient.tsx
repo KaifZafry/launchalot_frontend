11aa79d247061d31a7789b4536a29320448205da
KaifZafry <kaifzafry3110@gmail.com>
2026-01-19 11:57:46 +0530

    updated the disclaimer api structure and remove lib from gitignore

--- src/app/survey/surveyClient.tsx
+++ src/app/survey/surveyClient.tsx
@@ -11,22 +11,22 @@ export default function SurveyClient({ uiConfig }: Props) {
     <div
       className="min-h-screen bg-cover bg-center"
       style={{
-        backgroundImage: `url(${uiConfig.backgroundImage})`
+        backgroundImage: `url(${uiConfig?.config?.backgroundImage})`
       }}
     >
       <div className="p-6 max-w-md mx-auto bg-white/80 rounded">
         <label className="flex gap-2 items-center">
           <input type="checkbox" />
-          <span>{uiConfig.checkbox.text}</span>
+          <span>{uiConfig?.config?.checkbox?.text}</span>
         </label>
 
         <p className="text-xs mt-4 text-gray-600">
-          {uiConfig.disclaimer.text}
+          {uiConfig?.config?.disclaimer?.text}
         </p>
 
         <div className="mt-6 flex justify-center">
           <img
-            src={uiConfig.poweredBy.logo}
+            src={uiConfig?.config?.poweredBy?.logo}
             alt="Powered by"
             className="h-8"
           />
